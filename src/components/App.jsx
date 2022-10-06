02d48f47059133f7d7239a346a49ca52a85572f0
MarioDev1 <mariofuentes1904@gmail.com>
2022-10-06 16:43:11 -0300

    Segunda preentrega-MarioFuentes

---
+++ src/components/App.jsx
@@ -0,0 +1,31 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import '../styles/App.css'
+import Navbar from './content/Navbar';
+import Carrito from './content/Carrito';
+import ItemListContainer from './content/ItemListContainer';
+import About from './content/About';
+import Contacto from './content/Contacto';
+import ItemDetailContainer from './content/ItemDetailContainer';
+import Footer from './content/Footer'
+import Categoria from './content/Categoria';
+const App = () => {
+
+    return (
+        <>
+          <BrowserRouter>
+            <Navbar/>
+            <Routes>
+              <Route path="/" element={<ItemListContainer/>} />
+              <Route path="/producto/:id" element={<ItemDetailContainer/>} />
+              <Route path="/categoria/:id" element={<Categoria/>} />
+              <Route path="/about" element={<About/>} />
+              <Route path="/contacto" element={<Contacto/>} />
+              <Route path="/carrito" element={<Carrito/>} />
+            </Routes>
+            <Footer/>
+          </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/content/ItemDetailContainer.jsx
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react';
+import DetalleProducto from './DetalleProducto'
+import { useParams } from 'react-router-dom';
+import { consultarBDD } from '../../utils/funcionesUtiles';
+import '../../styles/cardProducto.css'
+const ItemDetailContainer = () => {
+    const [producto, setProducto] = useState([]);
+    const {id} = useParams()
+    useEffect(() => {
+       consultarBDD('../json/productos.json').then(productos => {
+        const producto1 = productos.find(productoArray => productoArray.id == id)
+        setProducto(producto1)
+       })
+   
+    }, [])
+
+    return (
+        
+            <div className="card  cardProducto mb-3" style={{maxWidth: '540px'}}>
+                <DetalleProducto producto={producto}/>
+            </div>
+
+       
+    );
+}
+
+export default ItemDetailContainer;
